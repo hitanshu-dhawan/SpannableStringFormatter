de8eb6138a85d495db77517ba86e05b41ad4e5b9
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-08-23 18:19:29 +0530

    test commit

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,5 @@
 import add from "../src/index";
 
 test("Test add function", () => {
-  expect(add(1, 1)).toBe(2);
-  expect(add(1, 2)).toBe(3);
-  expect(add(2, 1)).toBe(3);
+  expect(add(1, 1)).toBe(2); expect(add(1, 2)).toBe(3); expect(add(2, 1)).toBe(3);
 });
