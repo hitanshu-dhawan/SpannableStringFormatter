04d5ec622df6d942b7410cff3f05afb807069b92
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-09-05 18:28:25 +0530

    Added test cases

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,12 +1,70 @@
 import { SpannableStringFormatter } from "../src/index";
 
-test("test", () => {
-  expect(
-    SpannableStringFormatter.text("SpannableStringParser")
-      .property("text-color", "#0000FF")
-      .property("text-decoration", "underline", "strike-through")
-      .toString()
-  ).toBe(
-    "{ `SpannableStringParser` <text-color:`#0000FF`;text-decoration:`underline`|`strike-through`/> }"
-  );
+describe("text having no properties or no values", () => {
+  test("text having no property", () => {
+    expect(SpannableStringFormatter.text("Hitanshu Dhawan").toString()).toBe(
+      "Hitanshu Dhawan"
+    );
+  });
+
+  test("text having single property with no value", () => {
+    expect(
+      SpannableStringFormatter.text("Hitanshu Dhawan")
+        .property("text-color")
+        .toString()
+    ).toBe("Hitanshu Dhawan");
+  });
+
+  test("text having multiple properties with no values", () => {
+    expect(
+      SpannableStringFormatter.text("Hitanshu Dhawan")
+        .property("text-color")
+        .property("text-decoration")
+        .toString()
+    ).toBe("Hitanshu Dhawan");
+  });
+});
+
+describe("text having single property", () => {
+  test("text having single property with single value", () => {
+    expect(
+      SpannableStringFormatter.text("Hitanshu Dhawan")
+        .property("text-color", "#0000FF")
+        .toString()
+    ).toBe("{ `Hitanshu Dhawan` <text-color:`#0000FF`/> }");
+  });
+
+  test("text having single property with multiple values", () => {
+    expect(
+      SpannableStringFormatter.text("Hitanshu Dhawan")
+        .property("text-decoration", "underline", "strike-through")
+        .toString()
+    ).toBe(
+      "{ `Hitanshu Dhawan` <text-decoration:`underline`|`strike-through`/> }"
+    );
+  });
+});
+
+describe("text having multiple properties", () => {
+  test("text having multiple properties with single values", () => {
+    expect(
+      SpannableStringFormatter.text("Hitanshu Dhawan")
+        .property("text-color", "#0000FF")
+        .property("text-decoration", "underline")
+        .toString()
+    ).toBe(
+      "{ `Hitanshu Dhawan` <text-color:`#0000FF`;text-decoration:`underline`/> }"
+    );
+  });
+
+  test("text having multiple properties with multiple values", () => {
+    expect(
+      SpannableStringFormatter.text("Hitanshu Dhawan")
+        .property("text-color", "#0000FF")
+        .property("text-decoration", "underline", "strike-through")
+        .toString()
+    ).toBe(
+      "{ `Hitanshu Dhawan` <text-color:`#0000FF`;text-decoration:`underline`|`strike-through`/> }"
+    );
+  });
 });
